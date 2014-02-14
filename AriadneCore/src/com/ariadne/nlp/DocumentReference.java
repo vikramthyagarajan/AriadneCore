f3db49fb7571ebdb30a4a6d3e3bf97a527597f8f
Vikram Thyagarajan <viksdegod@gmail.com>
2014-02-15 00:10:26 +0530

    Set up Document Referencing

--- AriadneCore/src/com/ariadne/nlp/DocumentReference.java
+++ AriadneCore/src/com/ariadne/nlp/DocumentReference.java
@@ -1,5 +1,32 @@
 package com.ariadne.nlp;
 
-public class DocumentReference {
-
+public class DocumentReference 
+{
+	private String docID;
+	private int sentenceNo;
+	public DocumentReference(String docId,int sentenceNo)
+	{
+		this.docID=docId;
+		this.sentenceNo=sentenceNo;
+	}
+	public void setDocumentId(String docId)
+	{
+		this.docID=docId;
+	}
+	public String getDocumentId()
+	{
+		return this.docID;
+	}
+	public void setSentenceNo(int no)
+	{
+		this.sentenceNo=no;
+	}
+	public int getSentenceNo()
+	{
+		return this.sentenceNo;
+	}
+	public String toString()
+	{
+		return "DocumentID: "+docID+" || Sentence Number: "+sentenceNo;
+	}
 }
