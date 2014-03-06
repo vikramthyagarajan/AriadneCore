112795e4b248fb007795b35a11777025fb45f20b
Vikram Thyagarajan <viksdegod@gmail.com>
2014-03-06 22:37:19 +0530

    Shifted to units, where it should be.

---
+++ AriadneCore/src/com/ariadne/units/DocumentReference.java
@@ -0,0 +1,36 @@
+package com.ariadne.units;
+
+public class DocumentReference 
+{
+	private String docID;
+	private int sentenceNo;
+	public DocumentReference()
+	{
+		this.docID="";
+	}
+	public DocumentReference(String docId,int sentenceNo)
+	{
+		this.docID=docId;
+		this.sentenceNo=sentenceNo;
+	}
+	public void setDocumentId(String docId)
+	{
+		this.docID=docId;
+	}
+	public String getDocumentId()
+	{
+		return this.docID;
+	}
+	public void setSentenceNo(int no)
+	{
+		this.sentenceNo=no;
+	}
+	public int getSentenceNo()
+	{
+		return this.sentenceNo;
+	}
+	public String toString()
+	{
+		return "DocumentID: "+docID+" || Sentence Number: "+sentenceNo;
+	}
+}
