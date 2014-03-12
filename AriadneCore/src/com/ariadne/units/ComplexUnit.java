24e12de00607c27b527c979717b7c8e81b7ac3e9
Vikram Thyagarajan <viksdegod@gmail.com>
2014-03-12 12:37:20 +0530

    Verb made a string. Complex units do not hold arraylist of verbs anymore.

--- AriadneCore/src/com/ariadne/units/ComplexUnit.java
+++ AriadneCore/src/com/ariadne/units/ComplexUnit.java
@@ -4,19 +4,18 @@ import java.util.ArrayList;
 
 public class ComplexUnit 
 {
-	ArrayList<String>subjects,verbs,objects;
+	ArrayList<String>subjects,objects;
+	String verb;
 	ArrayList<Doublet>additionalData;
 	public ComplexUnit() 
 	{
 		subjects=new ArrayList<String>();
-		verbs=new ArrayList<String>();
 		objects=new ArrayList<String>();
 		additionalData=new ArrayList<Doublet>();
 	}
 	public ComplexUnit(ComplexUnit compUnit)
 	{
 		subjects=new ArrayList<String>(compUnit.getSubjects());
-		verbs=new ArrayList<String>(compUnit.getVerbs());
 		objects=new ArrayList<String>(compUnit.getObjects());
 		additionalData=new ArrayList<Doublet>(compUnit.getAdditionalData());
 	}
@@ -24,9 +23,9 @@ public class ComplexUnit
 	{
 		subjects.add(subj);
 	}
-	public void addVerb(String verb)
+	public void setVerb(String verb)
 	{
-		this.verbs.add(verb);
+		this.verb=verb;
 	}
 	public void addObject(String obj)
 	{
@@ -39,11 +38,7 @@ public class ComplexUnit
 	public int getSubjectCount()
 	{
 		return subjects.size();
-	}	
-	public int getVerbCount()
-	{
-		return verbs.size();
-	}	
+	}
 	public int getObjectCount()
 	{
 		return objects.size();
@@ -56,17 +51,29 @@ public class ComplexUnit
 	{
 		return this.subjects;
 	}
-	public ArrayList<String>getVerbs()
+	public String getSubjectAt(int i)
 	{
-		return this.verbs;
+		return subjects.get(i);
+	}
+	public String getVerb()
+	{
+		return this.verb;
 	}
 	public ArrayList<String>getObjects()
 	{
 		return this.objects;
 	}
+	public String getObjectAt(int i)
+	{
+		return objects.get(i);
+	}
 	public ArrayList<Doublet>getAdditionalData()
 	{
 		return this.additionalData;
+	}
+	public Doublet getAdditionalDataAt(int i)
+	{
+		return additionalData.get(i);
 	}
 	public String toString()
 	{
@@ -74,9 +81,8 @@ public class ComplexUnit
 		res.append("Subjects: ");
 		for(String s:subjects)
 			res.append(s+" ");
-		res.append("\nVerbs: ");
-		for(String s:verbs)
-			res.append(s+" ");
+		res.append("\nVerb: ");
+		res.append(verb+" ");
 		res.append("\nObjects: ");
 		for(String s:objects)
 			res.append(s+" ");
